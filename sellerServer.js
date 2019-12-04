1720f921ea3999b76cea93b366169bc96716c54f
clement <debaosuidecl@gmail.com>
2019-12-04 11:36:00 +0100

    d

--- sellerServer.js
+++ sellerServer.js
@@ -42,7 +42,9 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static('clientseller/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(
+      path.resolve(__dirname, 'clientseller', 'build', 'index.html')
+    );
   });
 }
 //set port
